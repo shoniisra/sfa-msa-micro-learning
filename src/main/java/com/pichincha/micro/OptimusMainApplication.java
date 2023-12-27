4c1ea68a9649a866c547d1ec23ef00e415a1a1d9
Johnny Israel <jivillac@pichincha.com>
2023-12-27 17:55:02 -0500

    empty micro back

---
+++ src/main/java/com/pichincha/micro/OptimusMainApplication.java
@@ -0,0 +1,33 @@
+package com.pichincha.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Arquitectura tradicional para el recurso spf-msa-micro-learning extendiendo
+ * de la interfaz generada.
+<br/>
+ * <b>Class</b>: PichinchaOptimusMainApplication<br/>
+ * <b>Copyright</b>: &copy; 2023 Banco Pichincha<br/>
+ *
+ * @author Banco Pichincha <br/>
+ * <u>Developed by</u>: <br/>
+ * <ul>
+*
+<li>hugo</li>
+*
+</ul>
+ * <u>Changes</u>:<br/>
+ * <ul>
+*
+<li>Dec 21, 2023 Creaci&oacute;n de Clase.</li>
+*
+</ul>
+ * @version 1.0
+ */
+@SpringBootApplication
+public class OptimusMainApplication {
+
+  public static void main(String[] args) {
+        SpringApplication.run(OptimusMainApplication.class, args);
+    }
+}
